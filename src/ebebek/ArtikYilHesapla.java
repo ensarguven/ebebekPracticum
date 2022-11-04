468a3882be5dc8dbb90c371fcc9a3608aedf17c0
Ensar Guven <ensarguvenofficial@gmail.com>
2022-11-04 16:33:38 +0300

    artik yil hesapla

---
+++ src/ebebek/ArtikYilHesapla.java
@@ -0,0 +1,47 @@
+package ebebek;
+
+import java.util.Scanner;
+
+public class ArtikYilHesapla {
+	public static void main(String[] args) {
+		
+		int yil;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Yýl Giriniz : ");
+		yil = input.nextInt();
+
+		// oncelikle 100'un kati olup olmadigini kontrol ediyoruz.
+		double hesap1 = yil % 100;
+		System.out.println(+hesap1);
+
+		if (hesap1 == 0) {
+
+			// 100'un kati ise 400'e tam bolunup bolunmedigine bakiyoruz.
+			double hesap2 = yil % 400;
+
+			if (hesap2 == 0) {
+
+				System.out.println(yil + " bir artik yildir ! ");
+			} else {
+
+				System.out.println(yil + " bir artik yil degildir ! ");
+			}
+
+		} else {
+
+			// 100'un kati degil ise 4'e tam bolunup bolunmedigine bakýyoruz.
+			double hesap3 = yil % 4;
+
+			if (hesap3 == 0) {
+
+				System.out.println(yil + " bir artýk yildir ! ");
+			} else {
+
+				System.out.println(yil + " bir artýk yil degildir ! ");
+			}
+		}
+	}
+
+}
