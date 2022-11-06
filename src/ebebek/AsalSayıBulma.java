660584114938ba5514978c7127b6ec9167909918
Ensar Guven <ensarguvenofficial@gmail.com>
2022-11-06 14:11:33 +0300

    first commit

---
+++ src/ebebek/AsalSayıBulma.java
@@ -0,0 +1,36 @@
+package ebebek;
+
+public class AsalSayýBulma {
+
+	public static void main(String[] args) {
+		
+		System.out.println("asal bulma");
+		
+		 for(int i=2; i<100; i++) {
+				int jump = 0; 
+
+			 for(int n=2; n<i; n++) {
+
+				 if(i%n==0) {
+					 jump=1;
+					 //System.out.print(i+" ");
+					 break;
+				 }		 
+			 
+		 }
+			 if(jump==0) {
+				 System.out.print(i+" ");
+				 }
+			 
+			
+	 }
+		
+			 
+ }
+			
+		}
+		
+
+	
+
+
