bd6a8707a2dfc7b1455399a96fc649e03643fb56
Ensar Guven <ensarguvenofficial@gmail.com>
2022-11-06 14:17:15 +0300

    düzen olusturuldu

--- src/ebebek/AsalSayıBulma.java
+++ src/ebebek/AsalSayıBulma.java
@@ -7,28 +7,23 @@ public class AsalSayýBulma {
 		System.out.println("asal bulma");
 		
 		 for(int i=2; i<100; i++) {
-				int jump = 0; 
+			 int jump = 0; 
 
 			 for(int n=2; n<i; n++) {
 
 				 if(i%n==0) {
-					 jump=1;
-					 //System.out.print(i+" ");
-					 break;
-				 }		 
-			 
-		 }
-			 if(jump==0) {
+					 jump=1;  //asal olmadýðý anlaþýlýyor
+					 break;  //ic döngüden cýkýyoruz.
+				 }			 
+			 }
+			 if(jump==0) {  
 				 System.out.print(i+" ");
-				 }
-			 
-			
-	 }
+			 }		 
 		
-			 
+		 }
+				 
+	}
  }
-			
-		}
 		
 
 	
