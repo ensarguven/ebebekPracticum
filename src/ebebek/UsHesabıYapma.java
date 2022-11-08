a2d8e97c89ff17191ad2845142212bd80f354c73
Ensar Guven <ensarguvenofficial@gmail.com>
2022-11-08 23:42:55 +0300

    Merge branch 'master' of https://github.com/ensarguven/ebebekPracticum.git

--- src/ebebek/UsHesabıYapma.java
+++ src/ebebek/UsHesabıYapma.java
@@ -4,6 +4,33 @@ import java.util.Scanner;
 
 public class UsHesabýYapma {
 	
+	static int recursiv(int x, int y) {
+		if(x>0)
+		{
+			x--;
+			return y * recursiv(x, y);
+		}
+		else {
+		      return 1;
+		    }
+		
+		
+	}
+	public static void main(String[] args) {
+		
+		Scanner input = new	Scanner(System.in);
+		int us;
+		int sayi;
+		
+		System.out.println("us giriniz : ");
+		us = input.nextInt();
+		System.out.println("us alýnacak sayiyi giriniz: ");
+		sayi = input.nextInt();
+		
+		System.out.println(recursiv(us, sayi));
+		
+	}
+	
 
 
 }
