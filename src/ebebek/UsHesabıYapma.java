ef24df2efbeebf2fe6d13875d50789547cac9bd1
Ensar Guven <ensarguvenofficial@gmail.com>
2022-11-08 20:36:16 +0300

    first

--- src/ebebek/UsHesabıYapma.java
+++ src/ebebek/UsHesabıYapma.java
@@ -3,17 +3,7 @@ package ebebek;
 import java.util.Scanner;
 
 public class UsHesabıYapma {
-
 	
-	   static int as(int i) {
-	        if (i < 2) {
-	        	return 3;
-	        	}
-	        else return (i * as(i - 1));
-	    }
 
-	    public static void main(String[] args) {
-	        System.out.println(as(3));
-	    }
 
 }
