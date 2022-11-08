c1c31ebae04ee6eea20a5f9f39dc3913ab83ef62
Ensar Guven <ensarguvenofficial@gmail.com>
2022-11-08 20:12:06 +0300

    first

---
+++ src/ebebek/FibonacciSerisi.java
@@ -0,0 +1,30 @@
+package ebebek;
+import java.util.Scanner;
+
+public class FibonacciSerisi {
+
+	public static void main(String[] args) {
+		int x;
+		int a=0,b=1,sonuc;
+		Scanner input = new Scanner(System.in);
+		System.out.print("eleman girin: ");
+		x = input.nextInt();
+		
+		for(int i=0; i<x; i++) {
+			if(i==0) {
+				System.out.print(a+" "+b+" ");
+			} 
+				sonuc = a+b;
+				a=b;
+				//System.out.println(b);
+				b=sonuc;
+				System.out.print(sonuc+" ");
+			
+			
+		}
+
+		
+
+	}
+
+}
